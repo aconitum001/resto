6bcd0fb567ae7d7cf955f58092cd06e84880df08
aconitum001 <skandern0000@gmail.com>
2024-10-06 09:55:53 +0100

    created appStyles file & added font

--- lib/app_styles.dart
+++ lib/app_styles.dart
@@ -1,7 +1,46 @@
 import 'dart:ui';
 
 abstract class AppStyles {
-  
+  static TextStyle styleRegular8 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 8),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle styleSemiBold16 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 16),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleRegular6 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 6),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle styleSemiBold12 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 12),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleSemiBold14 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 14),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle styleBold14 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 14),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle styleRegular10 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 10),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle styleRegular14 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 14),
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.normal,
+  );
 }
 
 double getResponsiveFontSize({required double fontsize}) {
