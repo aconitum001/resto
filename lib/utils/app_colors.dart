acf469e915adaefe7b7a8ec7144e74f6e180a78b
aconitum001 <skandern0000@gmail.com>
2024-10-06 10:32:08 +0100

    created approuter & colors file

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color textColor = Color(0xff3D3014);
+  static const Color primaryColor = Color(0xffEEBB4D);
+}
---
+++ lib/utils/app_router.dart
@@ -0,0 +1,12 @@
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => HomeScreen(),
+      ),
+    ],
+  );
+}
